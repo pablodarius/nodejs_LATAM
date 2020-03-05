d12404f9166cf68b7d7a68836301e5a801b73ffe
pablopd <pablopd@meta4.com>
2020-03-05 10:24:52 +0100

    xml metadata new folder and code changes

--- __tests__/03_example.metada.xml.spec.ts
+++ __tests__/03_example.metada.xml.spec.ts
@@ -2,7 +2,7 @@ import { M4TestUtils } from "@automation/m4nodejs";
 
 async function example(){
     const m4TestUtils = new M4TestUtils();
-    await m4TestUtils.createXmlMetadataFile("http://rdserverw10.meta4.com:40060", "M4ADM", "M4ADM","SCB_PY_LR_AUTOLIQUIDACION","C:/Temp");
+    await m4TestUtils.createXmlMetadataFile("http://rdserverw10.meta4.com:40060", "M4ADM", "M4ADM","SCB_PY_LR_AUTOLIQUIDACION",".\\__tests__\\XMLMetadata");
 }
 
 example();
